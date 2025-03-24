50f3ac2964571cfb3776ee8b684b3732075ed5ae
Tri Pham <fuinyss1@gmail.com>
2025-03-24 19:11:32 +0700

    init project

---
+++ CDWeb/src/test/java/com/example/CDWeb/CdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CDWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
