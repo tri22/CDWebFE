3cc80fca45b0b22749c94af7f4af6094c5a7e223
Tri Pham <fuinyss1@gmail.com>
2025-06-16 18:47:12 +0700

    fix

--- my-react-app/src/pages/LogManagement.jsx
+++ my-react-app/src/pages/LogManagement.jsx
@@ -7,7 +7,7 @@ import PaginationCom from "../components/PaginationCom";
 import { FaArrowDown } from "react-icons/fa";
 import { GrPowerReset } from "react-icons/gr";
 import { useTranslation } from 'react-i18next';
-
+import { toast } from "react-toastify";
 const LogManagement = () => {
     const [record, setRecord] = useState([])
     const { t, i18n } = useTranslation();
@@ -28,7 +28,10 @@ const LogManagement = () => {
             console.log(response.data.result)
             setRecord(response.data.result)
         } catch (err) {
-            console.error("Error loading data", err);
+            if (err.response) {
+                const errMessage = error.response.data?.message || 'Đã xảy ra lỗi';
+                toast.error(errMessage);
+            }
         }
 
     }
